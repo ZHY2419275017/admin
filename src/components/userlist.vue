1410274a1fbf5eb2545e6cceeefbe44b857a37e1
zhaohuiyu <2419275017@qq.com>
2019-08-27 10:48:02 +0800

    20190826

--- src/components/userlist.vue
+++ src/components/userlist.vue
@@ -15,7 +15,7 @@
 					</tr>
 				</thead>
 				<tbody>
-					<tr v-for="user in users" >
+					<tr v-for="user in page.list" >
 						<td>{{user.username}}</td>
 						<td>{{user.email}}</td>
 						<td>{{user.question}}</td>
@@ -35,7 +35,13 @@
 				</tbody>
 			</table>
 
-
+     <ul class="pagination" >
+      <li ><a class="btn" v-bind:class="{disabled:page.pageNum==1}" href="javascript:void(0)" v-on:click="prePage()">上一页</a></li>
+       <template v-for="item in page.pages">
+         <li v-bind:class="{active:item==page.pageNum}"><a href="javascript:void(0)" v-on:click="goPage(item)">{{item}}</a></li>
+       </template>
+      <li><a class="btn" v-bind:class="{disabled:page.pageNum==page.pages}" href="javascript:void(0)"  v-on:click="nextPage()">下一页</a></li>
+    </ul>
 
   </div>
 </template>
@@ -45,7 +51,7 @@ export default {
   name: 'userlist',
   data () {
     return {
-       users:[]
+       page:''
     }
   },
   created:function(){
@@ -53,15 +59,21 @@ export default {
   },
   methods:{
     getList:function(){
-      this.$http.get("http://192.168.0.23:8085/admin/selectAllUser").then(
+      this.$http.get("http://192.168.0.23:8085/admin/selectAllUser",{
+        params:{
+          pageNum:1,
+          pageSize:6
+        }
+      }).then(
         function(result){
-          console.log(result.body);
-          this.users=result.body
+          // console.log(result.body);
+          this.page=result.body
         },function(){
           alert("请求失败");
         }
       )
     },
+
     del:function(id){
       var y = confirm("确定要删除该用户吗？")
       if(y==true){
@@ -73,10 +85,11 @@ export default {
             this.getList();
             alert("删除成功");
         },function(error){
-          alert(error);
+          alert("请求失败");
         })
       }
     },
+
     updateUser:function(id){
        this.$router.push({
           path:"/updateUser",
@@ -84,13 +97,49 @@ export default {
             userId:id
           }
        })
-    }
+    },
+    prePage:function(){
+      //当前页码减一后重新发送请求
+       this.$http.get("http://192.168.0.23:8085/admin/selectAllUser",{
+         params:{
+           pageNum:this.page.pageNum-1,
+           pageSize:this.page.pageSize
+         }
+       }).then(function(result){
+          this.page=result.body
+       },function(error){
+          alert("请求失败")
+       })
+    },
+    goPage:function(item){
+         this.$http.get("http://192.168.0.23:8085/admin/selectAllUser",{
+          params:{
+            pageNum:item,
+            pageSize:this.page.pageSize
+          }
+        }).then(function(result){
+           this.page=result.body
+        },function(error){
+           alert("请求失败")
+        })
 
+    },
+    nextPage:function(){
+        this.$http.get("http://192.168.0.23:8085/admin/selectAllUser",{
+          params:{
+            pageNum:this.page.pageNum+1,
+            pageSize:this.page.pageSize
+          }
+        }).then(function(result){
+           this.page=result.body
+        },function(error){
+           alert("请求失败")
+        })
+    },
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
